090feabfb7f960334dfbc7faee9562dadb149e2c
nmastracci <mastracci.natalie@gmail.com>
2018-03-09 21:05:25 -0500

    initial strusture set

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var xhb = require('express-handlebars');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', xhb({ defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +24,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(require('./routes/index'));
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var connect = require('../utils/sqlConnect');
+var bodyparser = require('body-parser');
+var config = require('../config');
+
+var toRender = (config.kidsmode) ? 'main_kids' : 'home';
+router.use(bodyParser.urlencoded({extended:false}));
+router.use(bodyParser.json());
+//the middlest wares 
+router.use((req,resp,next) => {
+	var now = new Date();
+	var timestamp = now.toLocaleString('en-us', {
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true
+	});
+	console.log(`you made a ${req.method} call!`);
+	console.log(`you made the call at ${timestamp}`);
+	// console.log(req);
+	next();
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Newflix',
+  	message: 'Like Netflix but Worse',
+  	mainpage: true,
+  	kidsmode: config.kidsmode 
+  });
 });
+
+
+
+
 
 module.exports = router;
